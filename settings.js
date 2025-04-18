b801dfaca80ce02f033660a270d95d8ca7b8d7f8
ayik <kojibcell@gmail.com>
2025-04-18 18:42:41 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "ArBot",
+  botOwner: 'Ayik Kiya', // Your name
+  ownerNumber: '6285691994709', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "Bot buat iseng-iseng dan mainan saja.",
   version: "1.0.0",
 };
 
